3fdce2409e0280933719280df97ec74487910e97
KurtD13 <135623252+KurtD13@users.noreply.github.com>
2025-04-18 09:43:18 +0800

    Fix

---
+++ EcomsWeb/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import Landingpage from './Landingpage'
+
+function App() {
+
+  return (
+    <Landingpage/>
+  )
+}
+
+export default App
---
+++ EcomsWeb/src/Landingpage.jsx
@@ -0,0 +1,6 @@
+function Landingpage(){
+    return(
+        <h1>Landing Page</h1>
+    );
+}
+export default Landingpage
